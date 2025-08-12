3a24f74f7e5d6620a2f06df8a26799653a27e1d1
Pedro <rti-pedro@mac.mynetworksettings.com>
2025-08-12 18:24:39 -0400

    Prepare for GitHub and Netlify deployment

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
